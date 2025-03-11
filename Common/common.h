ffa761c3429ef194ec986759b4260e7151e0d6a5
chriswickens <chriswickens@gmail.com>
2025-03-11 13:56:20 -0400

    Added message stuff to the common.h

--- Common/common.h
+++ Common/common.h
@@ -17,12 +17,14 @@
 
 #define MAX_DC_ROLES 10
 
+// DCInfo struct
 typedef struct
 {
     pid_t dcProcessID;
     int lastTimeHeardFrom;
 } DCInfo;
 
+// MasterList struct
 typedef struct
 {
     int msgQueueID;
@@ -30,5 +32,27 @@ typedef struct
     DCInfo dc[MAX_DC_ROLES];
 } MasterList;
 
+// Messages Enum (0 to 6)
+typedef enum
+{
+    MSG_OKAY = 0,
+    MSG_HYDRAULIC_PRESSURE_FAILURE = 1,
+    MSG_SAFETY_BUTTON_FAILURE = 2,
+    MSG_NO_RAW_MATERIAL = 3,
+    MSG_TEMPERATURE_OUT_OF_RANGE = 4,
+    MSG_OPERATOR_ERROR = 5,
+    MSG_MACHINE_OFFLINE = 6,
+} MessageType;
+
+// Message Strings to be used when sending/logging messages
+const char *MessageStrings[] = {
+    "Everything is OKAY",                 // MSG_OKAY
+    "Hydraulic Pressure Failure",         // MSG_HYDRAULIC_PRESSURE_FAILURE
+    "Safety Button Failure",              // MSG_SAFETY_BUTTON_FAILURE
+    "No Raw Material in the Process",     // MSG_NO_RAW_MATERIAL
+    "Operating Temperature Out of Range", // MSG_TEMPERATURE_OUT_OF_RANGE
+    "Operator Error",                     // MSG_OPERATOR_ERROR
+    "Machine is Off-line"                 // MSG_MACHINE_OFFLINE
+};
 
 #endif // COMMON_H
