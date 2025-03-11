c15677eda5f26358a8c1d24718abf7f2a392f85d
chriswickens <chriswickens@gmail.com>
2025-03-11 06:48:09 -0400

    Some minor changes.

--- DC/mainDC.c
+++ DC/mainDC.c
@@ -1,11 +1,6 @@
 #include "../Common/common.h"
 #include "../Common/logWriter.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+
 
 int main(void)
 {
